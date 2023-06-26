1238737d28f0e6ce6935d82e8582b5d0e8493043
hxrish <108225503+hxrish@users.noreply.github.com>
2023-06-26 20:16:47 +0530

    Boilerplate timline

---
+++ src/timeline.js
@@ -0,0 +1,14 @@
+let pastContainer = document.createElement('div');
+pastContainer.id = 'pastContainer';
+pastContainer.innerText = 'HULULULUKKUKUJKU';
+
+let presentContainer = document.createElement('div');
+presentContainer.id = 'presentContainer';
+presentContainer.innerText = 'HOWAOWOAHwa';
+
+let futureContainer = document.createElement('div');
+futureContainer.id = 'futureContainer';
+futureContainer.innerText = 'JKBSOGBOSG';
+ 
+
+export {pastContainer, presentContainer, futureContainer};

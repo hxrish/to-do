ab041f074d7651751f393075dd6c55ff22a87bfa
hxrish <108225503+hxrish@users.noreply.github.com>
2023-06-28 15:25:04 +0530

    Add project additionn tab

---
+++ src/projectAddBody.js
@@ -0,0 +1,43 @@
+import { heroContainer } from ".";
+import { popupContainer } from "./popup";
+
+let projectForm = document.createElement('form');
+projectForm.id = "projectForm";
+
+let projectNameContainer = document.createElement('div');
+projectNameContainer.id = "projectNameContainer";
+
+let projectName = document.createElement('input');
+projectName.id = "projectName";
+
+let projectButtonContainer = document.createElement('div');
+projectButtonContainer.id = "projectButtonContainer";
+
+let projectSubmitBtn = document.createElement('button');
+projectSubmitBtn.id = "projectSubmitBtn";
+projectSubmitBtn.type = 'button';
+projectSubmitBtn.innerText = 'Submit';
+
+let projectCloseBtn = document.createElement('button');
+projectCloseBtn.id = "projectCloseBtn";
+projectCloseBtn.type = 'button';
+projectCloseBtn.innerText = 'Close';
+
+
+
+projectNameContainer.appendChild(projectName);
+projectButtonContainer.appendChild(projectSubmitBtn);
+projectButtonContainer.appendChild(projectCloseBtn);
+
+projectForm.appendChild(projectNameContainer);
+projectForm.appendChild(projectButtonContainer);
+
+projectCloseBtn.addEventListener('click', () => {
+
+    heroContainer.removeChild(popupContainer);
+
+})
+
+export {projectForm}
+
+

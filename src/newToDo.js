b096c855c44779e99a8016fba8bc1e7226ce81ae
hxrish <108225503+hxrish@users.noreply.github.com>
2023-06-28 15:24:41 +0530

    Todo Element

--- src/newToDo.js
+++ src/newToDo.js
@@ -1,5 +1,7 @@
 import { rightBottom} from ".";
 
+import Frame from './Frame.svg';
+
 function createElement(){
     let elementContainer = document.createElement('div');
     elementContainer.classList.add('elementContainer');
@@ -12,12 +14,24 @@ function createElement(){
     elementTitle.classList.add('elementTitle');
     elementTitle.innerText = 'holo';
 
+    let elementDetails = document.createElement('div');
+    elementDetails.id = 'elementDetails';
+
+    let trashbinContainer = document.createElement('div');
+    trashbinContainer.id = 'trashbinContainer';
+
+    const trashbin = new Image();
+    trashbin.id = 'trashbin';
+    trashbin.src = Frame;
+
+    trashbinContainer.appendChild(trashbin);
 
     elementContainer.appendChild(checkCircle);
     elementContainer.appendChild(elementTitle);
+    elementContainer.appendChild(elementDetails);
+    elementContainer.appendChild(trashbinContainer);
 
     return rightBottom.appendChild(elementContainer);
-
 }
 
 export {createElement};
